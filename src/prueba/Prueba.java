d3ce19a5beeed244f0680ba263b98952a91c8917
angeldmadrid4 <angeldmadrid4@penguin>
2025-07-04 09:18:04 -0600

    agregar system.out

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -14,7 +14,9 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      System.out.println("Hola mundo");
+      
+      System.out.println("Chrissy");
     }
     
 }
